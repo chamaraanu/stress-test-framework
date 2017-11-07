8542fd48f41df86fcf435d47ac83ceac11807106
chamaraanu <chamaraanu@yahoo.com>
2017-11-07 13:45:21 +0000

    addtional functionality

---
+++ src/test/java/org/wso2/stress/StressTest.java
@@ -0,0 +1,12 @@
+package org.wso2.stress;
+
+import org.testng.annotations.Test;
+
+
+public class StressTest {
+
+    @Test
+    public void exampleStress() {
+        System.out.println("This is a stress test!");
+    }
+}
