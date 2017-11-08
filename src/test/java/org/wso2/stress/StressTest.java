241f4e37c2d12a46ed599fc9771f0bb49d30084a
chamaraanu <chamaraanu@yahoo.com>
2017-11-08 05:18:01 +0000

    fixes for the NPE at org.apache.jmeter.engine.StandardJMeterEngine.configure(StandardJMeterEngine.java:164)

--- src/test/java/org/wso2/stress/StressTest.java
+++ src/test/java/org/wso2/stress/StressTest.java
@@ -6,6 +6,7 @@ import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -17,16 +18,17 @@ public class StressTest {
     public void runJmeter() throws Exception {
         StandardJMeterEngine jmeter = new StandardJMeterEngine();
 
-        JMeterUtils.setJMeterHome("/home/chamara/Programs/apache-jmeter-3.2 ");
-        JMeterUtils.loadJMeterProperties("/home/chamara/stress-test-framework/src/test/resources/CUSTOM/user.properties");
+        JMeterUtils.setJMeterHome("/home/chamara/Programs/apache-jmeter-3.2");
+        JMeterUtils.loadJMeterProperties("/home/chamara/Programs/apache-jmeter-3.2/bin/jmeter.properties");
         JMeterUtils.initLogging();
         JMeterUtils.initLocale();
 
         SaveService.loadProperties();
 
-        FileInputStream in = new FileInputStream("/home/chamara/stress-test-framework/src/test/jmeter/CUSTOM/Concurrency_Thread_Group.jmx");
+        //FileInputStream in = new FileInputStream("/home/chamara/stress-test-framework/src/test/jmeter/CUSTOM/Concurrency_Thread_Group.jmx");
+        File in = new File("/home/chamara/stress-test-framework/src/test/jmeter/CUSTOM/Concurrency_Thread_Group.jmx");
         HashTree testPlanTree = SaveService.loadTree(in);
-        in.close();
+        //in.close();
 
         jmeter.configure(testPlanTree);
         jmeter.run();
